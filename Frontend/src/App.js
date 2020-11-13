42b59f62052f2576a3f8515e5400ab8b8d9ec5d3
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-11-13 19:47:56 +0200

    Interface after login

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,83 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
+import SignUP from './component/SignUP'
+import English from './component/English'
+import Search from './component/Search'
+import Languages from './component/Languages'
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    console.log("It's running");
+  }, [])
+
   return (
-    <div className="App">
-      hi
-    </div>
+    <Router>
+      <Route exact path="/">
+        <div className="App">
+          <div className="head">
+            <div className="logo-cover"><h2 className="logo">Logo</h2></div>
+            <Search />
+            <Link to="/SignUP" ><div className="signUp-cover"><h2 className="signUP">SignUP</h2></div> </Link>
+          </div>
+          <div className="row">
+            <div className="column">
+              <div className="card">
+                <Link to="/english"><h2 className="english">English</h2></Link>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <h3>Card 2</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <h3>Card 3</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <h3>Card 3</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <h3>Card 3</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="card">
+                <h3>Card 4</h3>
+                <p>Some text</p>
+                <p>Some text</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Route>
+      <Route exact path="/english" component={English} />
+      <Route exact path="/SignUP" component={SignUP} />
+
+
+    </Router>
+
   );
 }
 
---
+++ Frontend/src/component/SignUP.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Redirect, } from 'react-router-dom';
+
+
+function SignUP() {
+
+  return (
+    <Router>
+      <div>11</div>
+    </Router>
+  )
+}
+
+export default SignUP;
