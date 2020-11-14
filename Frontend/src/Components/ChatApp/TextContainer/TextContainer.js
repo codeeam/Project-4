0b476b00861114b59f53d18eafab6270f894e914
tetra-shayeb <mohammednm40@gmail.com>
2020-11-14 13:17:47 +0200

    Finsh Text Container css & js

--- Frontend/src/Components/ChatApp/TextContainer/TextContainer.js
+++ Frontend/src/Components/ChatApp/TextContainer/TextContainer.js
@@ -2,26 +2,26 @@ import React from 'react';
 import onLineIcon from '../Icons/onlineIcon.png';
 import './TextContainer.css';
 
-const TextContainer = ({users}) => (
+const TextContainer = ({ users }) => (
     <div className="textContainer">
         {
             users
-            ?(
-                <div>
-                    <h1>Connected Pepole</h1>
-                    <div className="activeContainer">
-                        <h2>
-                            {users.map(({name}) =>(
-                                <div key={name} className="activeItem">
-                                    {name}
-                                    <img src={onLineIcon} alt="Online"/>
-                                </div>
-                            ))}
-                        </h2>
+                ? (
+                    <div>
+                        <h1>Connected Pepole</h1>
+                        <div className="activeContainer">
+                            <h2>
+                                {users.map(({ name }) => (
+                                    <div key={name} className="activeItem">
+                                        {name}
+                                        <img src={onLineIcon} alt="Online" />
+                                    </div>
+                                ))}
+                            </h2>
+                        </div>
                     </div>
-                </div>
-            )
-            :null
+                )
+                : null
         }
     </div>
 )
