5e1fdb00ec742799a4c015b97982dad1668f6412
tetra-shayeb <mohammednm40@gmail.com>
2020-11-14 14:04:24 +0200

    Finsh the file Message.js

---
+++ Frontend/src/Components/ChatApp/Messages/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
+import './Messages.css';
+
+const Messages = ({ messages, name }) =>  (
+    <ScrollToBottom className="messages">
+        {messages.map((message, i) => <div key={i}><Message message={message} name={name}></Message></div>)}
+    </ScrollToBottom>
+);
+
+export default Messages
