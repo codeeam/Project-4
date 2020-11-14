a10b1a1164bf76edac405fbf5a7469f978097001
tetra-shayeb <mohammednm40@gmail.com>
2020-11-14 13:12:39 +0200

    creat a Filles and install sum packages and end Text container.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import English from './Screens/English/English';
+import Join from './Components/ChatApp/Join/Join'
+import Chat from './Components/ChatApp/Chat/chat'
+
 
 function App() {
 	return (
+		
 		<div className="App">
 			<English />
-		</div>
+		</div>,
+		<Router>
+        <Route path="/" exact component={Join}></Route>
+        <Route path="/chat" component={Chat}></Route>
+    </Router>
+
+		
 	);
 }
 
