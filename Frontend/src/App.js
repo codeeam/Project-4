16ecb2a0cf268ad753d24ea39a2a2bd9624cec59
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-11-15 15:44:08 +0200

    fix English Link end add endbar that has About as and Contact us Link

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
-import SignUP from './component/SignUP'
-import English from './component/English'
-import Search from './component/Search'
-// import Languages from './component/Languages'
-
-
+import English from './Screens/English/English'
+import Navbar from './component/NavbarAfterLogin/Navbar'
 import './App.css';
-import English from './Screens/English/English';
 import Join from './Components/ChatApp/Join/Join'
 import Chat from './Components/ChatApp/Chat/chat'
+import About from './component/AboutUs/About'
+import Contact from './component/AboutUs/Contact'
 
 
 function App() {
@@ -23,16 +20,14 @@ function App() {
       <Route exact path="/">
         <div className="App">
           <div className="head">
-            <div className="logo-cover"><h2 className="logo">Logo</h2></div>
-            <Search />
-            <Link to="/SignUP" ><div className="signUp-cover"><h2 className="signUP">SignUP</h2></div> </Link>
+            <Navbar />
           </div>
           <div className="row">
             <div className="column">
               <div className="card">
-                <Link to="/english"><h2 className="english">English</h2></Link>
+                <Link to="/english" style={{ textDecoration: 'none' }} className="english1"><h3>English</h3></Link>
                 <p>Some text</p>
-                <p>Some text</p>
+                <p>Some text</p> 
               </div>
             </div>
 
@@ -76,25 +71,23 @@ function App() {
               </div>
             </div>
           </div>
+          <div className="bottom-stuff">
+            <div className="bottom">
+                
+              <div className="about-us"><Link to="/aboutUs" style={{ textDecoration: 'none' }}><h3>About Us</h3></Link></div>
+              <div className="contact-us"><Link to="/contactUs" style={{ textDecoration: 'none' }}><h3>Contact Us</h3></Link> </div>
+            </div>
+          </div>
         </div>
       </Route>
-      <Route exact path="/english" component={English} />
-      <Route exact path="/SignUP" component={SignUP} />
-
-
+      <Route exact path="/aboutUs" component={About}/>
+      <Route exact path="/contactUs" component={Contact}/>
+      <Route exact path="/english" component={English}/>
+      <Route path="/joinChat" exact component={Join}/>
+      <Route path="/chat" component={Chat}/>
     </Router>
- 
+
   );
-	return (
-		
-		<Router>
-			<Route path="/" exact component={English}></Route>
-			<Route path="/joinChat" exact component={Join}></Route>
-			<Route path="/chat" component={Chat}></Route>
-		</Router>
-
-
-	);
 }
 
 export default App;
