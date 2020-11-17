7956cdc598856ee445b5b25d82012ce1d8c10013
tetra-shayeb <mohammednm40@gmail.com>
2020-11-17 22:51:08 +0200

    Add button to chat & fixed some err

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {  useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Languages from './Screens/Languages/Languages'
@@ -7,8 +7,8 @@ import English from './Screens/English/English'
 
 import Login from './Screens/Login/Login';
 import SignUp from './Screens/SignUp/SignUp'
-// import Join from './Components/ChatApp/Join/Join'
-// import Chat from './Components/ChatApp/Chat/chat'
+import Join from './Components/ChatApp/Join/Join'
+import Chat from './Components/ChatApp/Chat/chat'
 import About from './Components/AboutUs/About'
 import Contact from './Components/ContactUs/Contact'
 
@@ -16,16 +16,14 @@ import Contact from './Components/ContactUs/Contact'
 function App() {
   return (
     <Router>
-      <Route  path="/Languages/English" exact component={English}/>
+      <Route path="/Languages/English" exact component={English} />
       <Route path="/Languages" exact component={Languages} />
       <Route path="/aboutUs" exact component={About} />
       <Route path="/contactUs" exact component={Contact} />
-      
       <Route path="/" exact component={Login} />
       <Route path="/signup" exact component={SignUp} />
-      {/*
-      <Route path="/joinChat" exact component={Join}/>
-      <Route path="/chat" component={Chat}/>*/}
+      <Route path="/joinChat" exact component={Join} />
+      <Route path="/chat" component={Chat} />
     </Router>
   );
 }
