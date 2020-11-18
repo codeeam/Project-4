a9e09e0d411007cef602822ad295536bac4dca96
Mohammad-W-Momani <mohammad.w.momani@gmail.com>
2020-11-18 23:25:22 +0200

    grammer

---
+++ Frontend/src/Components/EnglishComponent/Navbar/EnglishCourses/Listening/ListeningNavbar/ListeningNavbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./GrammerNavbar.css";
+import TranslateIcon from "@material-ui/icons/Translate";
+import HomeIcon from "@material-ui/icons/Home";
+
+const Navbar = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <TranslateIcon style={{ fontSize: 50, color: "#2e81f4" }} />
+      </div>
+      <div className="header__center">
+        <header style={{fontSize:"24px"}}>English Grammar Courses</header>
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
