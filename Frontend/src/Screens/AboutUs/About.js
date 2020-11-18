3f6419d40473d84e1041d6f7100abe66526c27de
Mohammed-khamis <moh.o.khamis@gmail.com>
2020-11-18 09:47:02 +0200

    aboutUs

---
+++ Frontend/src/Screens/AboutUs/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './../../Components/AboutUs/Navbar/Navbar'
+import Feed from './../../Components/AboutUs/Feed/Feed'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <Feed />
+        </div>
+    )
+}
